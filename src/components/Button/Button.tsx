e706c47d672b011270492174a0f05dff1e60823c
Bast22 <patriciaheimfarth@gmail.com>
2023-03-26 14:42:04 +0200

    using styled

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,37 +1,91 @@
-
 import React from "react";
 import classNames from "classnames";
 import "./Button.css";
-
+import styled from "@emotion/styled";
+import { css, keyframes } from "@emotion/react";
+import img from "../../../stories/assets/stars.png";
 export interface ButtonProps {
-    type?: "primary" | "secondary";
-    textColor?: string;
-    size?: "small" | "medium" | "large";
-    label: string;
-    onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  type?: "primary" | "secondary";
+  textColor?: string;
+  color?: string;
+  size?: "small" | "medium" | "large";
+  label: string;
+  opacity: number;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
+const bg = keyframes`
+0% {
+    background-position: 0% 33%;
+  }
+  33% {
+    background-position: 50% 33%;
+  }
+
+  66% {
+    background-position: 66% 66%;
+  }
+  88% {
+    background-position: 44% 88%;
+  }
+  100% {
+    background-position: 66% 100%;
+  }
+}
+`;
+
+const StyledButton = styled.button`
+  & {
+    color: black;
+    background: pink;
+    position: relative;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    font-family: Nunito Sans, Helvetica Neue, Helvetica, Arial, sans-serif;
+    font-weight: 700;
+    cursor: pointer;
+    display: inline-block;
+    line-height: 1;
+  }
+
+  &:before {
+    content: "";
+    background-size: 200% 500%;
+    animation: ${bg} 0.3s infinite;
+    animation-direction: normal;
+    background-image: url(${img});
+    position: absolute;
+    opacity: 0.4;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
+`;
 
 const Button = ({
-    type = "primary",
-    textColor,
-    size = "medium",
-    onClick,
-    label,
+  type = "primary",
+  textColor,
+  color,
+  size = "medium",
+  onClick,
+  label,
+  opacity,
 }: ButtonProps) => {
-    return (
-        <button
-            type="button"
-            className={classNames(
-                "storybook-button",
-                `storybook-button--${size}`,
-                `storybook-button--${type}`
-            )}
-            style={textColor ? { color: textColor } : {}}
-            onClick={onClick}
-        >
-            {label}
-        </button>
-    );
+  return (
+    <StyledButton
+      type="button"
+      className={classNames(
+        "storybook-button",
+        `storybook-button--${size}`,
+        `storybook-button--${type}`
+      )}
+      onClick={onClick}
+    >
+      {label}
+    </StyledButton>
+  );
 };
 
 export default Button;
